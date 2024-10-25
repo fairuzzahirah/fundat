779f5755a9b0ca45b86675c03ea7c87cfc970043
flowsel <50051799+flowsel@users.noreply.github.com>
2024-10-25 10:03:22 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
-Route::post('login', [LoginController::class, 'login']);
-Route::post('register', [RegisterController::class, 'register']);
-Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+// Group routes with middleware
+Route::middleware(['api'])->group(function () {
+    Route::post('login', [LoginController::class, 'login']);
+    Route::post('register', [RegisterController::class, 'register']);
+    Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+});
